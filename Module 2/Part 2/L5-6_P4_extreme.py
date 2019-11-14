7b05786a4199b4b7bd21b49fbedc7ab896684a3c
malak <malakabdou@cmail.carleton.ca>
2019-11-14 11:32:06 -0500

    extreme

---
+++ Module 2/Part 2/L5-6_P4_extreme.py
@@ -0,0 +1,31 @@
+from Cimpl import *
+file = choose_file()
+original_image = load_image(file)
+
+def extreme_contrast(original_image: Image) -> Image:
+    """Returns the 'extreme contrast' version of an image without having modified it.
+    - Function written by Malak Abdou - 101139692
+    
+    >>> extreme_contrast(original_image)
+    <Cimpl.Image object at 0x00000212B29BD948>
+    """
+    extreme_copy = copy(original_image)
+    r, g, b = get_color(extreme_copy, 0, 0)
+    for pixel in extreme_copy:
+        x, y, (r, g, b) = pixel
+        if r<=127:
+            r=0
+        else:
+            r=255
+        if g<=127:
+            g=0
+        else:
+            g=255
+        if b<=127:
+            b=0
+        else:
+            b=255
+        extreme_color = create_color(r,g,b)
+        set_color (extreme_copy, x, y, extreme_color)            
+    return extreme_copy
+extreme = extreme_contrast(original_image)
