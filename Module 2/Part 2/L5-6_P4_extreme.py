09b8c73bd8229aae6282f6650156c9c1ddba2ccb
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-14 20:49:06 -0500

    Formatted extreme filter

--- Module 2/Part 2/L5-6_P4_extreme.py
+++ Module 2/Part 2/L5-6_P4_extreme.py
@@ -1,6 +1,8 @@
 from Cimpl import *
+
 file = choose_file()
 original_image = load_image(file)
+
 
 def extreme_contrast(original_image: Image) -> Image:
     """Returns the 'extreme contrast' version of an image without having modified it.
@@ -13,19 +15,21 @@ def extreme_contrast(original_image: Image) -> Image:
     r, g, b = get_color(extreme_copy, 0, 0)
     for pixel in extreme_copy:
         x, y, (r, g, b) = pixel
-        if r<=127:
-            r=0
+        if r <= 127:
+            r = 0
         else:
-            r=255
-        if g<=127:
-            g=0
+            r = 255
+        if g <= 127:
+            g = 0
         else:
-            g=255
-        if b<=127:
-            b=0
+            g = 255
+        if b <= 127:
+            b = 0
         else:
-            b=255
-        extreme_color = create_color(r,g,b)
-        set_color (extreme_copy, x, y, extreme_color)            
+            b = 255
+        extreme_color = create_color(r, g, b)
+        set_color(extreme_copy, x, y, extreme_color)
     return extreme_copy
+
+
 extreme = extreme_contrast(original_image)
