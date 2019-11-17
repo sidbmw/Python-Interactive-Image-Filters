896edbe1eb0dd38b0912cebce6f2f7d3debc4adb
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-16 23:54:47 -0500

    WIP

--- Module 2/Part 2/L5_6_P4_test_two_tone.py
+++ Module 2/Part 2/L5_6_P4_test_two_tone.py
@@ -5,18 +5,21 @@ file = choose_file()
 original_image = load_image(file)
 
 
-def test_two_tone(img):
-    img = create_image(3, 1)  # creates image
-    set_color(img, 0, 0, create_color(0, 0, 0))
-    set_color(img, 1, 0, create_color(128, 128, 128))
-    set_color(img, 2, 0, create_color(255, 255, 255))
-    test_img = copy(img)
+def testThreeTone(test_image):
 
-    for col in colours:
-        print('\n testing', col)
-        test_img = threeTone(test_img, col, col, col)
-        for x, y, (r, g, b) in test_img:
-            if (r, g, b) == col:
-                print('pass ', end='')
-            else:
-                print('fails at', col, r, g, b)
+    black = create_color(0, 0, 0)
+    white = create_color(255, 255, 255)
+    red = create_color(255, 0, 0)
+    lime = create_color(0, 255, 0)
+    blue = create_color(0, 0, 255)
+    yellow = create_color(255, 255, 0)
+    cyan = create_color(0, 255, 255)
+    magenta = create_color(255, 0, 255)
+    gray = create_color(128, 128, 128)
+
+    for pixel in test_image:
+        x, y, (r, g, b) = pixel
+        
+
+
+print(testThreeTone(original_image))
