1d194937879b06ee44df3d85cf8337517e79ff63
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-17 16:42:56 -0500

    done 2 tone test?

--- Module 2/Part 2/L5_6_P4_test_two_tone.py
+++ Module 2/Part 2/L5_6_P4_test_two_tone.py
@@ -1,25 +1,39 @@
 from Cimpl import *
-from two_tone import *
+from L5_6_P4_two_tone import two_tone as tt
 
 file = choose_file()
 original_image = load_image(file)
 
 
-def testThreeTone(test_image):
+# two_tone(original_image, "black", "white")
 
-    black = create_color(0, 0, 0)
-    white = create_color(255, 255, 255)
-    red = create_color(255, 0, 0)
-    lime = create_color(0, 255, 0)
-    blue = create_color(0, 0, 255)
-    yellow = create_color(255, 255, 0)
-    cyan = create_color(0, 255, 255)
-    magenta = create_color(255, 0, 255)
-    gray = create_color(128, 128, 128)
 
-    for pixel in test_image:
+def test_two_tone():
+    filtered_image = tt(original_image, "black", "white")
+    test_state = True
+
+    for pixel in filtered_image:
         x, y, (r, g, b) = pixel
+        color_original = get_color(original_image, x, y)
+        color_filtered = get_color(filtered_image, x, y)
+        color_average = (color_original[0] + color_original[1] + color_original[2]) / 3
         
+        colour_1 = create_color(0, 0, 0)
+        colour_2 = create_color(255, 255, 255)
+
+        if color_average <= 127 and color_filtered == colour_1 or color_average >= 128 and color_filtered == colour_2:
+            test_state = True
+        else:
+            test_state = False
+
+    if test_state:
+        print('Test Passed')
+    else:
+        print('Test Failed')
+
+    return test_state
 
 
-print(testThreeTone(original_image))
+# show(tt(original_image, "black", "white"))
+
+test_two_tone()
