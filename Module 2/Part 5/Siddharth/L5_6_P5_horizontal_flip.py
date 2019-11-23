2ce043ec8546efef142e6f9c0f5ff5cb9dc4159b
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-22 23:25:01 -0500

    Edited horizontal flip

--- Module 2/Part 5/Siddharth/L5_6_P5_horizontal_flip.py
+++ Module 2/Part 5/Siddharth/L5_6_P5_horizontal_flip.py
@@ -1,3 +1,10 @@
+"""
+ECOR 1051 Fall 2019
+Group Number: L5-6
+
+Horizontal flip function
+"""
+
 from Cimpl import *
 
 file = choose_file()
@@ -5,25 +12,28 @@ original_image = load_image(file)
 
 
 def flip_horizontal(original_image: Image) -> Image:
-    # """ Author: Siddharth Natamai - 1011403016
-    #     Date: Nov 19, 2019
-    #
-    # Returns a original_image after flipping along the x-axis (horizontal flip)
-    # >>> flip_horizontal(original_image)
-    # <Cimpl.original_image object at 0x7f7ba88dbd10>
-    # """
-    img_width = get_width(original_image)
-    img_height = get_height(original_image)
+    """ Author: Siddharth Natamai - 1011403016
+        Date: Nov 19, 2019
+
+    Returns a original_image after flipping along the x-axis (horizontal flip)
+    >>> flip_horizontal(original_image)
+    <Cimpl.original_image object at 0x7f7ba88dbd10>
+    """
+
+    new_image = copy(original_image)
+    img_width = get_width(new_image)
+    img_height = get_height(new_image)
     img_center = img_height // 2
 
     for x in range(img_width):
         for y in range(img_center):
-            r, g, b = get_color(original_image, x, y)
-            r2, g2, b2 = get_color(original_image, x, img_height - y - 1)
-            set_color(original_image, x, y, create_color(r2, g2, b2))
-            set_color(original_image, x, img_height - y - 1, create_color(r, g, b))
+            r, g, b = get_color(new_image, x, y)
+            r2, g2, b2 = get_color(new_image, x, img_height - y - 1)
+            set_color(new_image, x, y, create_color(r2, g2, b2))
+            set_color(new_image, x, img_height - y - 1, create_color(r, g, b))
 
-    return original_image
+    return new_image
 
 
 show(flip_horizontal(original_image))
+# flip_horizontal(original_image)
