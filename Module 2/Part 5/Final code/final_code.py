f82e54d7c6fca225101e7481a3b78814f761c366
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-24 19:34:09 -0500

    Added Malak's code

--- Module 2/Part 5/Final code/final_code.py
+++ Module 2/Part 5/Final code/final_code.py
@@ -1,5 +1,5 @@
-from Cimpl import copy, get_width, get_height, get_color, set_color, \
-    create_color, Image
+from Cimpl import create_color, get_color, Image, load_image, show, \
+    choose_file, copy, set_color, get_height, get_width
 
 
 def _adjust_component(original_val: int) -> int:
@@ -198,3 +198,67 @@ def detect_edges_better(original_image: Image, threshold: float) -> Image:
                     set_color(edges_copy, x, y, white)
 
     return edges_copy
+
+
+def extreme_contrast(original_image: Image) -> Image:
+    """Returns the 'extreme contrast' version of an image without having
+    modified it.
+    - Function written by Malak Abdou - 101139692
+
+    >>> original_image = load_image(choose_file())
+    >>> show(extreme_contrast(original_image))
+    # Returns a copy of original_image with extreme_contrast filter on it.
+    """
+    extreme_copy = copy(original_image)
+    for pixel in extreme_copy:
+        x, y, (r, g, b) = pixel
+        if r <= 127:
+            r = 0
+        else:
+            r = 255
+        if g <= 127:
+            g = 0
+        else:
+            g = 255
+        if b <= 127:
+            b = 0
+        else:
+            b = 255
+        extreme_color = create_color(r, g, b)
+        set_color(extreme_copy, x, y, extreme_color)
+    return extreme_copy
+
+
+def detect_edges(original_image: Image, threshold: float) -> Image:
+    """Takes an image, makes a copy of it and changes the color of the pixels
+    to black or white, depending on their contrast.
+
+    - Function written by Malak Abdou - 101139692
+
+    >>> original_image = load_image(choose_file())
+    >>> show(detect_edges(original_image, 15))
+    # Returns image with detect_edges filter applied with a threshold of 15
+    without modifying original.
+    """
+
+    original2 = copy(original_image)
+
+    black = create_color(0, 0, 0)
+    white = create_color(255, 255, 255)
+
+    for x in range(get_width(original2)):
+
+        for y in range(get_height(original2) - 1):
+
+            r, g, b = get_color(original2, x, y)
+            r1, g1, b1 = get_color(original2, x, (y + 1))
+
+            brightness = (r + g + b) / 3
+            brightness1 = (r1 + g1 + b1) / 3
+
+            if abs(brightness - brightness1) > threshold:
+                set_color(original2, x, y, black)
+            else:
+                set_color(original2, x, y, white)
+
+    return original2
--- Module 2/Part 5/Final code/final_test_code.py
+++ Module 2/Part 5/Final code/final_test_code.py
@@ -1,9 +1,10 @@
-from Cimpl import set_color, \
-    create_image, get_color, create_color, Image
+from Cimpl import create_color, create_image, get_color, set_color, Image
 from L5_6_P4_extreme import extreme_contrast
+from L5_6_P4_sepia import sepia
 from L5_6_P4_three_tone import three_tone
 from L5_6_P4_two_tone import two_tone as tt
 from L5_6_P5_edge import detect_edges
+from L5_6_P5_imp_edge import detect_edges_better
 from test_grayscale import check_equal
 
 
@@ -43,7 +44,7 @@ def check_equal(description: str, outcome, expected) -> None:
     else:
         print("{0} PASSED".format(description))
     print("------")
-    
+
 
 def test_detect_edge() -> None:
     """Returns whether the edge detection filter works and if it didn't,\
@@ -301,3 +302,113 @@ def test_two_tone(original_image: Image):
         print('Test Failed')
 
     return test_state
+
+
+def test_sepia() -> None:
+    """Returns the information on whether or not the sepia filter works
+    properly or not. The function returns the pixels that have passed the
+    test. All pixels have to pass in order to confirm the success of the
+    sepia filter. Otherwise, the filter does not work properly.
+
+    - Function written by Malak Abdou - 101139692
+
+    >>> test_sepia()
+    (0,0) PASSED
+    (1,0) PASSED
+    (2,0) PASSED
+    (3,0) PASSED
+    (0,1) PASSED
+    (1,1) PASSED
+    (2,1) PASSED
+    (3,1) PASSED
+
+    References: check_equal function from fib2.py, provided by ECOR 1051
+    staff.
+    """
+    original = create_image(4, 2)
+
+    set_color(original, 0, 0, create_color(15, 250, 8))
+    set_color(original, 1, 0, create_color(8, 20, 11))
+    set_color(original, 2, 0, create_color(5, 55, 15))
+    set_color(original, 3, 0, create_color(32, 206, 56))
+
+    set_color(original, 0, 1, create_color(93, 51, 240))
+    set_color(original, 1, 1, create_color(11, 23, 41))
+    set_color(original, 2, 1, create_color(54, 26, 190))
+    set_color(original, 3, 1, create_color(21, 68, 49))
+
+    expected = create_image(4, 2)
+
+    set_color(expected, 0, 0, create_color(104.65, 91, 77.35))
+    set_color(expected, 1, 0, create_color(14.3, 13, 11.7))
+    set_color(expected, 2, 0, create_color(27.5, 25, 22.5))
+    set_color(expected, 3, 0, create_color(112.7, 98, 83.3))
+
+    set_color(expected, 0, 1, create_color(147.2, 128, 108.8))
+    set_color(expected, 1, 1, create_color(27.5, 25, 22.5))
+    set_color(expected, 2, 1, create_color(103.5, 90, 76.5))
+    set_color(expected, 3, 1, create_color(50.6, 46, 41.4))
+
+    sepia_image = sepia(original)
+
+    for x, y, color in sepia_image:
+        check_equal('(' + str(x) + ',' + str(y) + ')', color,
+                    get_color(expected, x, y))
+
+    def test_detect_edges_better() -> None:
+        """Returns the information on whether or not the filter
+        detect_edges_better works properly or not. The function returns the
+        pixels that have passed the test. All pixels have to pass in order to
+        confirm the success of the detect_edges_better filter. Otherwise, the
+        filter does not work properly.
+
+        -Function written by Malak Abdou - 101139692
+
+        >>> test_detect_edges_better()
+        (0,0) PASSED
+        (1,0) PASSED
+        (2,0) PASSED
+        (3,0) PASSED
+        (0,1) PASSED
+        (1,1) PASSED
+        (2,1) PASSED
+        (3,1) PASSED
+
+        References: check_equal function from fib2.py, provided by ECOR 1051
+        staff.
+        """
+
+        black = create_color(0, 0, 0)
+        white = create_color(255, 255, 255)
+
+        original = create_image(4, 2)
+
+        set_color(original, 0, 0, create_color(15, 250, 7))
+        set_color(original, 1, 0, create_color(8, 19, 10))
+        set_color(original, 2, 0, create_color(5, 55, 15))
+        set_color(original, 3, 0, create_color(32, 206, 56))
+
+        set_color(original, 0, 1, create_color(43, 50, 21))
+        set_color(original, 1, 1, create_color(11, 23, 40))
+        set_color(original, 2, 1, create_color(54, 26, 190))
+        set_color(original, 3, 1, create_color(21, 68, 48))
+
+        expected = create_image(4, 2)
+
+        set_color(expected, 0, 0, create_color(0, 0, 0))
+        set_color(expected, 1, 0, create_color(255, 255, 255))
+        set_color(expected, 2, 0, create_color(0, 0, 0))
+        set_color(expected, 3, 0, create_color(0, 0, 0))
+
+        set_color(expected, 0, 1, create_color(255, 255, 255))
+        set_color(expected, 1, 1, create_color(0, 0, 0))
+        set_color(expected, 2, 1, create_color(0, 0, 0))
+        set_color(expected, 3, 1, create_color(255, 255, 255))
+
+        threshold = 15  # Assume threshold chosen is 15.
+
+        edges_image = detect_edges_better(original, threshold)
+
+        for x, y, color in edges_image:
+            check_equal('(' + str(x) + ',' + str(y) + ')', color,
+                        get_color(expected, x, y))
