3c48f68125acdecd81a9c050d1c403c71a4ad111
Siddharth Natamai <siddharthnamtamai@gmail.com>
2019-11-24 19:07:10 -0500

    WIP

--- Module 2/Part 5/Final code/final_test_code.py
+++ Module 2/Part 5/Final code/final_test_code.py
@@ -1,6 +1,5 @@
-from Cimpl import copy, get_width, get_height, set_color, \
-    create_image
-from Cimpl import get_color, create_color, Image
+from Cimpl import set_color, \
+    create_image, get_color, create_color, Image
 from L5_6_P4_extreme import extreme_contrast
 from L5_6_P4_three_tone import three_tone
 from L5_6_P4_two_tone import two_tone as tt
@@ -10,6 +9,7 @@ from test_grayscale import check_equal
 
 def check_equal(description: str, outcome, expected) -> None:
     """
+    Author: Prof. Donald L. Bailey
     Print a "passed" message if outcome and expected have same type and
     are equal (as determined by the == operator); otherwise, print a
     "fail" message.
@@ -24,7 +24,6 @@ def check_equal(description: str, outcome, expected) -> None:
     type, which must be a type for which == is used to determine if two values
     are equal. Don't use this function to check if floats, lists of floats,
     tuples of floats, etc. are equal.
-    -Donald Bailey
     """
     outcome_type = type(outcome)
     expected_type = type(expected)
@@ -44,10 +43,10 @@ def check_equal(description: str, outcome, expected) -> None:
     else:
         print("{0} PASSED".format(description))
     print("------")
-
+    
 
 def test_detect_edge() -> None:
-    """Returns wether the edge detection filter works and if it did'nt,\
+    """Returns whether the edge detection filter works and if it didn't,\
     indicates where it wasn't applied.
     -Test function written by Leanne Matamoros - 10114740
 
@@ -187,69 +186,6 @@ def test_extreme() -> None:
     for x, y, col in extreme_image:
         check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
                     col, get_color(expected, x, y))
-
-
-def check_equal(description: str, outcome, expected) -> None:
-    """
-    Author: Prof. Donald L. Bailey
-    Print a "passed" message if outcome and expected have same type and
-    are equal (as determined by the == operator); otherwise, print a
-    "fail" message.
-
-    Parameter description should provide information that will help us
-    interpret the test results; e.g., the call expression that yields
-    outcome.
-
-    Parameters outcome and expected are typically the actual value returned
-    by a call expression and the value we expect a correct implementation
-    of the function to return, respectively. Both parameters must have the same
-    type, which must be a type for which == is used to determine if two values
-    are equal. Don't use this function to check if floats, lists of floats,
-    tuples of floats, etc. are equal.
-    """
-    outcome_type = type(outcome)
-    expected_type = type(expected)
-    if outcome_type != expected_type:
-
-        # The format method is explained on pages 119-122 of
-        # 'Practical Programming', 3rd ed.
-
-        print("{0} FAILED: expected ({1}) has type {2}, " \
-              "but outcome ({3}) has type {4}".
-              format(description, expected,
-                     str(expected_type).strip('<class> '),
-                     outcome, str(outcome_type).strip('<class> ')))
-    elif outcome != expected:
-        print("{0} FAILED: expected {1}, got {2}".
-              format(description, expected, outcome))
-    else:
-        print("{0} PASSED".format(description))
-    print("------")
-
-
-def flip_vertical(image: Image) -> Image:
-    # """Function author : Nathan Gomes - 101143780
-    # Function takes an image and returns a copy of the image that has been
-    # flipped vertically (across the "y" axis in an x-y co-ordinate system).
-    #
-    # >>> original_image = load_image("filename")
-    # >>> flip_vertical(original_image)
-    # <Cimpl.Image object at 0x000001A90D7A7408>
-    # """
-
-    new_image = copy(image)
-    mid_pixel = get_width(new_image) // 2
-    width = get_width(new_image)
-    height = get_height(new_image)
-
-    for x in range(mid_pixel):
-        for y in range(height):
-            r, g, b = get_color(image, x, y)
-            new_r, new_g, new_b = get_color(image, abs(width - x) - 1, y)
-            set_color(new_image, x, y, create_color(new_r, new_g, new_b))
-            set_color(new_image, width - x - 1, y, create_color(r, g, b))
-
-    return new_image
 
 
 def test_vertical_flip():
